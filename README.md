8334fec23a1ef8177edf4c3edd555a80fb8ca5f2
Martin Bångens <marbangens@gmail.com>
2022-01-01 22:40:31 +0100

    bashify license header

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Im gonna my upload slackware build scripts here and configuration tips and confi
 post_install.sh can be run after installing full slackware current from now to release of slackware 15.
 When slackware 15 comes around my slackbuilds will stick to that release. This is not yet usefull.
 
+Dont do anything before you run this, run this first thing you do after installation.
+Then create you're user and make sure you're in the audio group.
 
--- post_install.sh
+++ post_install.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-Slackware Studio
-Copyright (C) 2022  Martin Bångens
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+#   Slackware Studio
+#   Copyright (C) 2022  Martin Bångens
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 echo "heloo balloo"
+
 wget https://github.com/sbopkg/sbopkg/releases/download/0.38.1/sbopkg-0.38.1-noarch-1_wsr.tgz
 wget https://sourceforge.net/projects/slackpkgplus/files/slackpkg%2B-1.7.10-noarch-1mt.txz
 
