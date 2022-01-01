b54beb7779b2d916aad7b7621891ebb83c88ef36
Martin Bångens <marbangens@gmail.com>
2022-01-01 22:23:57 +0100

    last dummy test

--- post_install.sh
+++ post_install.sh
@@ -15,4 +15,12 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-echo "heloo balloo\n"
+echo "heloo balloo"
+wget https://github.com/sbopkg/sbopkg/releases/download/0.38.1/sbopkg-0.38.1-noarch-1_wsr.tgz
+wget https://sourceforge.net/projects/slackpkgplus/files/slackpkg%2B-1.7.10-noarch-1mt.txz
+
+# check md5 for pkgs
+# 42e40c32a7aed4c417f1ee852c51c6d0  sbopkg-0.38.1-noarch-1_wsr.tgz
+# d80f1d97fa3a2d8abb4cda72664f03c7  slackpkg+-1.7.10-noarch-1mt.txz
+
+# install and move configuration files
