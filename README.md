727347e6cf33babad866467739ff7343a94ab972
Martin Bångens <marbangens@gmail.com>
2022-01-02 11:33:37 +0100

    add simple md5sum check

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-Im gonna my upload slackware build scripts here and configuration tips and config files for slackware pro audio.
+# Slackware Studio
 
-post_install.sh can be run after installing full slackware current from now to release of slackware 15.
-When slackware 15 comes around my slackbuilds will stick to that release. This is not yet usefull.
+# How
+Run post_install.sh after installing full slackware-current for now until release of slackware 15.
+
+ - !!This is not yet usefull yet!!
 
 Dont do anything before you run this, run this first thing you do after installation.
-Then create you're user and make sure you're in the audio group.
+Then create you're user and make sure you're in the audio group and use slackware like normal.
+
+Will later add linux kernel compilation and configuration scripts and some script for
+recompiling slackware pkgs if there are problem for real-time audio or if I think some software is too old.
 
--- post_install.sh
+++ post_install.sh
@@ -7,22 +7,48 @@
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
-
+#
 #   This program is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
-
+#
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-echo "heloo balloo"
+# check md5 for pkgs
+sbopkg_md5sum="42e40c32a7aed4c417f1ee852c51c6d0  sbopkg-0.38.1-noarch-1_wsr.tgz"
+slackpkg_md5sum="d80f1d97fa3a2d8abb4cda72664f03c7  slackpkg+-1.7.10-noarch-1mt.txz"
 
+# Download
 wget https://github.com/sbopkg/sbopkg/releases/download/0.38.1/sbopkg-0.38.1-noarch-1_wsr.tgz
 wget https://sourceforge.net/projects/slackpkgplus/files/slackpkg%2B-1.7.10-noarch-1mt.txz
 
-# check md5 for pkgs
-# 42e40c32a7aed4c417f1ee852c51c6d0  sbopkg-0.38.1-noarch-1_wsr.tgz
-# d80f1d97fa3a2d8abb4cda72664f03c7  slackpkg+-1.7.10-noarch-1mt.txz
+# Check
+sbopkg_md5sum_dl=$(md5sum sbopkg-0.38.1-noarch-1_wsr.tgz)
+slackpkg_md5sum_dl=$(md5sum slackpkg+-1.7.10-noarch-1mt.txz)
 
-# install and move configuration files
+if [[ $sbopkg_md5sum == $sbopkg_md5sum_dl ]]
+    then
+        echo -e "\nsbopkg download seem okej"
+    else
+        echo -e "\nsbopkg download faild"
+        exit 1
+fi
+
+if [[ $slackpkg_md5sum == $slackpkg_md5sum_dl ]]
+    then
+        echo -e "\nslackpkg download seem okej"
+    else
+        echo -e "\nslackpkg download faild"
+        exit 1
+fi
+
+# Install and move configuration files
+
+
+
+# Clean up
+rm sbopkg-0.38.1-noarch-1_wsr.tgz
+rm slackpkg+-1.7.10-noarch-1mt.txz
+exit 0
